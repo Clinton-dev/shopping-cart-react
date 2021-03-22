5fca8d14303fb956bdaeded9db87c414a366a68e
clinton wambugu <clintonwambugu@gmail.com>
2021-03-22 22:36:42 +0300

    Add function to handle price and quantity

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Product(props) {
+  const [quantity, setQuantity] = useState(1);
+  const [amount, setAmount] = useState(props.prodPrice);
+
+  const addQuantity = () => {
+    setQuantity(quantity + 1);
+    handleAmount();
+  }
+
+ const removeQuantity = () => {
+    setQuantity(quantity - 1);
+    handleAmount(); 
+ }
+
+  const handleAmount = () => {
+    setAmount(amount * quantity);
+  }
 
   return (
     <div className="col">
@@ -8,8 +24,14 @@ function Product(props) {
         <img src={props.prodImage} className="card-img-top" alt={props.title} />
         <div className="card-body">
           <h5 className="card-title">{props.title}</h5>
-          <p className="card-text">KES {props.prodPrice}</p>
-          <a href="#" className="btn btn-primary" >Add to cart</a>
+          <p className="card-text">KES {amount}</p>
+          <div className="d-flex">
+            <a href="#" className="btn btn-primary" >Add to cart</a>
+              &nbsp; &nbsp; &nbsp;
+            <section>
+            <p><button className="btn btn-light" onClick={removeQuantity}>-</button> {quantity} <button className="btn btn-light" onClick={addQuantity}>+</button></p>
+            </section>
+          </div>
         </div>
       </div>
     </div>
