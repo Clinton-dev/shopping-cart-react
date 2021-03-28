065b2ea5ef12a5ac3515896d6a22e6e5fb9d3ece
clinton wambugu <clintonwambugu@gmail.com>
2021-03-28 21:57:19 +0300

    Make navbar to be position sticky

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Home() {
   return(
-    <div>
+    <div className="container">
       <h2>Offering class</h2>
       <p> with brands like Convers, Vans and Crocs </p>
     </div>
